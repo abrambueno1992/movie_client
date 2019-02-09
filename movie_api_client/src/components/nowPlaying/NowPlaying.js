2a17e2fbef52e9b12e6c04c2b7d50c695cb17baa
Abraham Bueno <abueno@localhost.localdomain>
2019-02-09 10:49:05 -0600

    Title rendered

--- movie_api_client/src/components/nowPlaying/NowPlaying.js
+++ movie_api_client/src/components/nowPlaying/NowPlaying.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { getNowPlaying } from "../../actions/nowPlaying";
+// css
+import "./NowPlaying.css";
 class NowPlaying extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      loading: true
+      loading: true,
+      movies: []
     };
   }
 
@@ -16,6 +18,10 @@ class NowPlaying extends Component {
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.now_playing !== this.props.now_playing) {
+      this.props.now_playing.results.forEach((movie, i) => {
+        // this.setState({})
+        console.log("movie", movie);
+      });
       this.setState({ loading: false });
     }
   }
@@ -25,14 +31,20 @@ class NowPlaying extends Component {
     if (this.state.loading === true) {
       return <div>Loading....</div>;
     } else {
-      this.props.now_playing.results.forEach((movie, i) => {
-        console.log("movie", movie);
-      });
       return (
         <div>
           <Link to="/popular">Popular</Link>
           <Link to="top-rated">Top Rated</Link>
           <h1>Now Playing</h1>
+          {this.props.now_playing.results.map((movie, i) => {
+            console.log("Inside, movie title:", movie.original_title);
+
+            return (
+              <div class="movie-details" key={movie + i}>
+                <h3>{movie.original_title}</h3>
+              </div>
+            );
+          })}
         </div>
       );
     }
