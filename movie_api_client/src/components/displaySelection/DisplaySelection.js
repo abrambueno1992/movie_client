f17bd5c458de2af62726a4d60a80897ffb6082c1
Abraham Bueno <abueno@localhost.localdomain>
2019-02-09 15:03:48 -0600

    Display component now renders data, and is ready to be worked on

--- movie_api_client/src/components/displaySelection/DisplaySelection.js
+++ movie_api_client/src/components/displaySelection/DisplaySelection.js
@@ -55,12 +55,40 @@ class DisplaySelection extends Component {
     const movie_index = this.props.match.url.split("/")[
       this.props.match.url.split("/").length - 1
     ];
+    const api_key = process.env.REACT_APP_API_KEY;
+    const urlBase = `https://image.tmdb.org/t/p/original`;
+    const urlBaseBackdrop = `https://image.tmdb.org/t/p/original`;
     console.log("PROPS of NOWPLYaying:", parseInt(movie_index, 10));
-    return (
-      <div>
-        <h1>Display selection</h1>
-      </div>
-    );
+    if (this.state.selection === "") {
+      return (
+        <div>
+          <h3>Loading...</h3>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Display selection</h1>
+          {this.state.selection.results.map((movie, i) => {
+            return (
+              <div key={movie + i}>
+                <h3>Title: {movie.title}</h3>
+                <img src={`${urlBase + movie.poster_path}`} />
+                <img src={`${urlBaseBackdrop + movie.backdrop_path}`} />
+                <div>Summary:</div>
+                <h4>{movie.overview}</h4>
+                <h5>Popularity: {movie.popularity}</h5>
+                <h5>
+                  Vote Average: {movie.vote_average} out of {movie.vote_count}{" "}
+                  votes
+                </h5>
+                <h5>Release Date: {movie.release_date}</h5>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
   }
 }
 const mapStateToProps = state => {
