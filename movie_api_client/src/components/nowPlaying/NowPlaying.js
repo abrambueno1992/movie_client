881de61ee9e875583d9632a287b57d3484de2361
Abraham Bueno <abueno@localhost.localdomain>
2019-02-09 11:24:44 -0600

    Backdrop image rendered

--- movie_api_client/src/components/nowPlaying/NowPlaying.js
+++ movie_api_client/src/components/nowPlaying/NowPlaying.js
@@ -29,6 +29,7 @@ class NowPlaying extends Component {
   render() {
     const api_key = process.env.REACT_APP_API_KEY;
     const urlBase = `https://image.tmdb.org/t/p/original`;
+    const urlBaseBackdrop = `https://image.tmdb.org/t/p/original`;
     console.log("Movies Now Playing: ", this.props.now_playing);
     if (this.state.loading === true) {
       return <div>Loading....</div>;
@@ -49,6 +50,7 @@ class NowPlaying extends Component {
               <div className="movie-details" key={movie + i}>
                 <h3>Title: {movie.original_title}</h3>
                 <img src={`${urlBase + movie.poster_path}`} />
+                <img src={`${urlBaseBackdrop + movie.backdrop_path}`} />
                 <div>Summary:</div>
                 <h4>{movie.overview}</h4>
                 <h5>Popularity: {movie.popularity}</h5>
